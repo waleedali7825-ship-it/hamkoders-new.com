55a6257dcbb943c19452ddaa912751580efb6d2a
waleedali7825-ship-it <waleedali7825@gmail.com>
2026-02-04 23:25:31 +0500

    change

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import "./Header.css";
 import Link from "next/link";
 import Image from "next/image";
-import LOGO_IMAGE from "../../../public/images/logo.webp";
+import LOGO_IMAGE from "../../../public/images/logo-1.png";
 
 const Header = () => {
   return (
